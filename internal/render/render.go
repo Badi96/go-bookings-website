1ddfbf140be8f86dc67d2c000d6990d26641fdcf
badi96 <badi.mirzai@gmail.com>
2024-09-19 21:02:25 +0300

    added tests for post and get handlers

--- internal/render/render.go
+++ internal/render/render.go
@@ -15,6 +15,7 @@ import (
 
 var functions = template.FuncMap{}
 var app *config.AppConfig
+var pathToTemplates = "./templates"
 
 // NewTemplates sets the config for the package
 func NewTamplates(a *config.AppConfig) {
@@ -66,7 +67,7 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 
 	mycache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	pages, err := filepath.Glob(fmt.Sprintf("%s/*.page.tmpl", pathToTemplates))
 	if err != nil {
 		return mycache, err
 	}
@@ -79,13 +80,13 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 			return mycache, err
 		}
 
-		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		matches, err := filepath.Glob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplates))
 		if err != nil {
 			return mycache, err
 		}
 
 		if len(matches) > 0 {
-			ts, err = ts.ParseGlob(("./templates/*.layout.tmpl"))
+			ts, err = ts.ParseGlob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplates))
 			if err != nil {
 				return mycache, err
 			}
